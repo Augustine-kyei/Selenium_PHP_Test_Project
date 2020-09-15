fd71765dd20a85bddfb6d851b780422d84839294
austi <austi@192.168.1.4>
2020-09-15 21:57:07 +0100

    done

---
+++ src/test/java/phpwebsite/NewTest.java
@@ -0,0 +1,42 @@
+package phpwebsite;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+
+public class NewTest {
+	WebDriver driver;
+	
+	@BeforeMethod
+	  public void beforeMethod() {
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("http://3.21.103.115:32768");
+		
+		
+	  }
+	
+	
+	
+  @Test
+  public void verify () {
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  driver.navigate().to(" http://3.21.103.115:32768/content/about-us.php");
+	  
+	  
+	  
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+  
+ 
+}
